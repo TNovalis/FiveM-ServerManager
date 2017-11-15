ca419a7476459e0a939ced016f88db2641615500
Tom <TNovalis@users.noreply.github.com>
2017-11-15 14:02:39 -0600

    increment version

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     /*
      * Here goes the application version.
      */
-    'version' => '0.0.7',
+    'version' => '0.0.8',
 
     /*
      * Here goes the application default command. By default
